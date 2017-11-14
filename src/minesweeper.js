81a2ac9ebead9345552b6d5be61d3196dfdac871
Jon Cino <joncino@Jons-MBP.fios-router.home>
2017-11-14 16:53:00 -0500

    first full commit

---
+++ src/minesweeper.js
@@ -0,0 +1,19 @@
+/* MAIN MINESWEEPER CODE FILE */
+/* Written by: Jon Cino */
+
+
+// Demo what an empty board would look like
+const blankLine = '   |   |   ';
+console.log('This is what an empty board would look like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+
+// Demo what a board of a real game might look like.
+const guessLine = ' 1 |   |   ';
+const bombLine = '   | B |   ';
+console.log('This is what a board with a guess and a bomb on it would look like');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
